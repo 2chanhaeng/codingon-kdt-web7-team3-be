654784b2971ac76217f4f6a3b48c45297255afed
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-04 12:14:35 +0900

    feat: 현재까지 구현된 기능 앱 모듈에 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { ConfigModule } from "@nestjs/config";
+import { UserModule } from "./user/user.module";
+import { LoginModule } from "./login/login.module";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ".env",
+      // process.env.NODE_ENV === "development" ? ".env.dev" : ".env.test",
+    }),
+    UserModule,
+    LoginModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
