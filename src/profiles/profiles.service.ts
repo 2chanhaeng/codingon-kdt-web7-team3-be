864cd3ebf56de6b4d39987068dbc9a8ba524076e
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-04 17:47:44 +0900

    feat: getByFollowersCountRankings 생성

--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@nestjs/common";
-import { Profile } from "@prisma/client";
+import { Profile, Prisma } from "@prisma/client";
 import { PrismaService } from "~/prisma.service";
 
 @Injectable()
@@ -9,6 +9,19 @@ export class ProfilesService {
   /** userId 유저의 모든 프로필 조회 */
   async getByUserId(userId: string): Promise<Profile[] | null> {
     const where = { userId };
-    return this.db.profile.findMany({ where });
+    return await this.db.profile.findMany({ where });
+  }
+
+  /** 팔로워 수 순으로 프로필 조회 */
+  async getByFollowersCountRankings(
+    cursorId: string | null,
+  ): Promise<Profile[] | null> {
+    const where = {};
+    const take = 10;
+    const skip = cursorId ? 1 : 0;
+    const cursor = cursorId ? { id: cursorId } : undefined;
+    const orderBy = { followers: { _count: Prisma.SortOrder.desc } };
+    const findManyArgs = { where, take, skip, cursor, orderBy };
+    return await this.db.profile.findMany(findManyArgs);
   }
 }
