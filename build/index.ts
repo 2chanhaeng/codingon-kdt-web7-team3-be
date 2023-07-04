7ab387fba07a5c7577f521e26f1806425743a0ec
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-04 13:53:13 +0900

    feat: .env 파일 생성 함수 생성

--- build/index.ts
+++ build/index.ts
@@ -1,3 +1,15 @@
+import { existsSync, writeFile } from "fs";
+
+const ENV_PATH = ".env";
+
 main();
 
-async function main() {}
+async function main() {
+  await createEnvFile();
+}
+
+async function createEnvFile() {
+  if (existsSync(ENV_PATH)) return;
+  writeFile(ENV_PATH, "", (err) => console.log(err));
+}
+
