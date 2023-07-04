e98df333c8de58a6f685c508edd24e9576ee4005
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-04 17:42:11 +0900

    feat: ProfilesService.getByUserId 생성

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
+import { PrismaService } from "~/prisma.service";
 import { ProfilesController } from "./profiles.controller";
 import { ProfilesService } from "./profiles.service";
 
 @Module({
   controllers: [ProfilesController],
-  providers: [ProfilesService],
+  providers: [ProfilesService, PrismaService],
 })
 export class ProfilesModule {}
--- src/profiles/profiles.service.ts
+++ src/profiles/profiles.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { Profile } from "@prisma/client";
+import { PrismaService } from "~/prisma.service";
 
 @Injectable()
-export class ProfilesService {}
+export class ProfilesService {
+  constructor(private readonly db: PrismaService) {}
+
+  /** userId 유저의 모든 프로필 조회 */
+  async getByUserId(userId: string): Promise<Profile[] | null> {
+    const where = { userId };
+    return this.db.profile.findMany({ where });
+  }
+}
