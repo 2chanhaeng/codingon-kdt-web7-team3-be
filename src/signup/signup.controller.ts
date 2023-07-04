a23a993c625e41bc832bb41892dd1fd635f30f6a
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-04 14:38:30 +0900

    feat: POST /signup 요청 처리

--- src/signup/signup.controller.ts
+++ src/signup/signup.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
+import { SignupService } from "./signup.service";
+import { CreateUserDto } from "~/user/user.dto";
 
 @Controller("signup")
-export class SignupController {}
+export class SignupController {
+  constructor(private readonly signupService: SignupService) {}
+
+  @Post()
+  async create(@Body() body: CreateUserDto) {
+    return this.signupService.create(body);
+  }
+}
