86885847fa13c531c681c8086f5df715964489c2
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-05 17:21:09 +0900

    feat: chats 모듈, 게이트웨이 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { LoginModule } from "./login/login.module";
 import { SignupModule } from "./signup/signup.module";
 import { ProfilesModule } from "./profiles/profiles.module";
 import { PrismaModule } from "./prisma/prisma.module";
+import { ChatsModule } from "./chats/chats.module";
 
 @Module({
   imports: [
@@ -18,6 +19,7 @@ import { PrismaModule } from "./prisma/prisma.module";
     SignupModule,
     ProfilesModule,
     PrismaModule,
+    ChatsModule,
   ],
   controllers: [],
   providers: [],
