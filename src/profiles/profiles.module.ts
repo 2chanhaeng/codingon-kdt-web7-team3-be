444d366611b3f8d241e4afac2749d470d3ece0e0
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-05 14:08:19 +0900

    feat: Profiles 모듈 Profile 컨트롤러 추가

--- src/profiles/profiles.module.ts
+++ src/profiles/profiles.module.ts
@@ -1,12 +1,12 @@
 import { Module } from "@nestjs/common";
 import { PrismaService } from "~/prisma.service";
-import { ProfilesController } from "./profiles.controller";
+import { ProfileController, ProfilesController } from "./profiles.controller";
 import { ProfilesService } from "./profiles.service";
 import { LoginModule } from "~/login/login.module";
 
 @Module({
   imports: [LoginModule],
-  controllers: [ProfilesController],
+  controllers: [ProfilesController, ProfileController],
   providers: [ProfilesService, PrismaService],
 })
 export class ProfilesModule {}
