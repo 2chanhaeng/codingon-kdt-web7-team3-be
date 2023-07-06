bf806ebd0c00edf86047bc1b497e58339ade3cc9
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-06 16:01:11 +0900

    feat: 사용하지 않는 코드 제거

--- src/chats/chats.module.ts
+++ src/chats/chats.module.ts
@@ -1,10 +1,7 @@
 import { Module } from "@nestjs/common";
-import { ChatsController } from "./chats.controller";
-import { ChatsService } from "./chats.service";
-import { ChatsGateway } from './chats.gateway';
+import { ChatsGateway } from "./chats.gateway";
 
 @Module({
-  controllers: [ChatsController],
-  providers: [ChatsService, ChatsGateway],
+  providers: [ChatsGateway],
 })
 export class ChatsModule {}
