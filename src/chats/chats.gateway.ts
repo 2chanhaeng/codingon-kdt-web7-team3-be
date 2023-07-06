1ffda5102d3086b3dd2c2479980f6b7296b55687
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-06 15:30:54 +0900

    feat: 게이트웨이 소켓 서버 추가

--- src/chats/chats.gateway.ts
+++ src/chats/chats.gateway.ts
@@ -4,11 +4,16 @@ import {
   OnGatewayInit,
   SubscribeMessage,
   WebSocketGateway,
+  WebSocketServer,
 } from "@nestjs/websockets";
+import { Server, Socket } from "socket.io";
+
 @WebSocketGateway(81, { transports: ["websocket"] })
 export class ChatsGateway
   implements OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit
 {
+  @WebSocketServer()
+  server: Server;
 
   @SubscribeMessage("message")
   handleMessage(client: any, payload: any): string {
