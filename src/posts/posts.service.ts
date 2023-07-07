c3d7174a9b0a9f7c2f9da33019faf3f29a95d5ab
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-07 14:22:57 +0900

    feat: create 생성

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { Prisma } from "@prisma/client";
+import { PrismaService } from "../prisma/prisma.service";
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.PostUncheckedCreateInput) {
+    return await this.prisma.post.create({
+      data,
+    });
+  }
+}
