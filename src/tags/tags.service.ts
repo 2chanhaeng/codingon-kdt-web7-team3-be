a402d74cd97b2071f469bc071c356810b5bee110
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 18:54:56 +0900

    feat: 서비스 constructor 정의

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,4 +1,8 @@
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "~/prisma/prisma.service";
 
 @Injectable()
-export class TagsService {}
+export class TagsService {
+  constructor(private readonly db: PrismaService) {}
+
+}
