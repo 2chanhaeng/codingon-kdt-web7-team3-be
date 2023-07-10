7d8865764d607e11a8f01e5e83724a2c7f1fb84a
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 16:27:45 +0900

    feat: 게시물 생성 컨트롤러 정의

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,9 +1,26 @@
-import { Controller } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import {
+  Body,
+  Controller,
+  Post,
+} from "@nestjs/common";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "~/jwt/jwt.guard";
+import { Jwt } from "~/jwt/jwt.decorator";
 import { PostsService } from "./posts.service";
+import { PostDto } from "./posts.dto";
 
 @ApiTags("Posts")
 @Controller("posts")
 export class PostsController {
   constructor(private readonly posts: PostsService) {}
+
+  /**
+   * `POST /posts`: 게시물 생성
+   */
+  @ApiBearerAuth("access")
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Jwt("profileId") id: string, @Body() body: PostDto) {
+    return this.posts.createPost(id, body);
+  }
 }
