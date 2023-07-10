cc071adf2b0ae679cb8b499224100577119c9fbd
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 15:48:02 +0900

    feat: 사용하지 않는 DTO 제거

--- src/jwt/jwt.dto.ts
+++ src/jwt/jwt.dto.ts
@@ -1,19 +1,6 @@
-import { IsString } from "class-validator";
 import { IntersectionType, PartialType } from "@nestjs/swagger";
-import { UserIdDto, ProfileIdDto, AccessDto } from "@/src/dto/property.dto";
+import { UserIdDto, ProfileIdDto, AccessDto } from "~/dto/property.dto";
 
-export class JwtLoginDto {
-  @IsString()
-  userId: string;
-}
-
-export class JwtPayloadDto {
-  @IsString()
-  userId: string;
-
-  @IsString()
-  profileId?: string;
-}
 export class JwtResDto extends AccessDto {}
 export class JwtUserDto extends UserIdDto {}
 export class JwtProfileDto extends IntersectionType(UserIdDto, ProfileIdDto) {}
