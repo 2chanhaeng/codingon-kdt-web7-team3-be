28442ff83b0597a6668798978b504e92a8a24494
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 15:20:11 +0900

    feat: JWT DTO 추가 정의
    똑같은 데이터라고 똑같은 DTO를 돌려쓰지 않고 상위 DTO를 상속 받아 맥락에 맞는 DTO의 이름을 지어줌으로써 코드 가독성 강화

--- src/jwt/jwt.dto.ts
+++ src/jwt/jwt.dto.ts
@@ -1,4 +1,6 @@
 import { IsString } from "class-validator";
+import { IntersectionType, PartialType } from "@nestjs/swagger";
+import { UserIdDto, ProfileIdDto, AccessDto } from "@/src/dto/property.dto";
 
 export class JwtLoginDto {
   @IsString()
@@ -12,3 +14,10 @@ export class JwtPayloadDto {
   @IsString()
   profileId?: string;
 }
+export class JwtResDto extends AccessDto {}
+export class JwtUserDto extends UserIdDto {}
+export class JwtProfileDto extends IntersectionType(UserIdDto, ProfileIdDto) {}
+export class JwtMaybeProfileDto extends IntersectionType(
+  UserIdDto,
+  PartialType(ProfileIdDto),
+) {}
