42724fe8a3094ef9567c2364d9cfbe4719d25e13
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 18:55:34 +0900

    feat: 태그 생성 서비스 정의

--- src/tags/tags.service.ts
+++ src/tags/tags.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from "@nestjs/common";
 import { PrismaService } from "~/prisma/prisma.service";
+import {
+  CreateDto,
+} from "./tags.dto";
 
 @Injectable()
 export class TagsService {
   constructor(private readonly db: PrismaService) {}
 
+  async create(data: CreateDto) {
+    const select = { id: true };
+    return await this.db.tag.create({ data, select });
+  }
+
+  async createTag(data: CreateDto) {
+    const tag = await this.create(data);
+    return { success: !!tag };
+  }
 }
