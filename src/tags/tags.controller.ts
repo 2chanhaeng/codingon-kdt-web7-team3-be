a1ff601326b57b5ae22e030f2a8972b19d8a457f
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 19:43:50 +0900

    feat: 태그 생성 컨트롤러 정의

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,11 +1,23 @@
 import {
   Controller,
+  Post,
 } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
+import { JwtAuthGuard } from "~/jwt/jwt.guard";
+import { Jwt } from "~/jwt/jwt.decorator";
 import { TagsService } from "./tags.service";
+import { CreateDto } from "./tags.dto";
 
 @ApiTags("Tags")
 @Controller("tags")
 export class TagsController {
   constructor(private readonly tags: TagsService) {}
+
+  /** `POST /tags`: 태그 생성 */
+  @ApiBearerAuth("access")
+  @UseGuards(JwtAuthGuard)
+  @Post()
+  async create(@Body() body: CreateDto) {
+    return await this.tags.createTag(body);
+  }
 }
