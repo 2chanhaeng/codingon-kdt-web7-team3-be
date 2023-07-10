d33cbfa03d5ad3ca3042205a9f3033314d6fd1df
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 19:42:43 +0900

    feat: 컨트롤러 constructor 정의

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from "@nestjs/common";
+import {
+  Controller,
+} from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { TagsService } from "./tags.service";
 
+@ApiTags("Tags")
 @Controller("tags")
-export class TagsController {}
+export class TagsController {
+  constructor(private readonly tags: TagsService) {}
+}
