291c1fdd42064a93848037ae2403dbcf6e2493b2
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 16:35:17 +0900

    feat: 유저 구독 게시물 조회 컨트롤러 정의

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,7 +1,10 @@
 import {
   Body,
   Controller,
+  Get,
   Post,
+  Query,
+  UseGuards,
 } from "@nestjs/common";
 import { ApiBearerAuth, ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "~/jwt/jwt.guard";
@@ -22,5 +25,18 @@ export class PostsController {
   @Post()
   async create(@Jwt("profileId") id: string, @Body() body: PostDto) {
     return this.posts.createPost(id, body);
+  }
+
+  /**
+   * `GET /posts`: 유저가 구독한 태그나 유저의 게시물 조회
+   */
+  @ApiBearerAuth("access")
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async getPostsOfSubscribesAndFollows(
+    @Jwt("profileId") id: string,
+    @Query("cursor") cursor: string,
+  ) {
+    return this.posts.readsSubscribesAndFollows(id, cursor);
   }
 }
