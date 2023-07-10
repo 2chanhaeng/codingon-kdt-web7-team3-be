595b33d8014fc4595bbc32e624a24727e84ce6b0
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 15:04:30 +0900

    feat: 앱에 swagger 추가
    swagger 설정 정의 후 main.ts에서 앱에 등록

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
 import { AppModule } from "./app.module";
+import { config as swaggerConfig } from "./app.swagger";
+import { SwaggerModule } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,7 +13,10 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup("api", app, document);
   app.enableCors();
+  // log all requests
   await app.listen(8000);
 }
 bootstrap();
