b942dc1a64d7ace498d2bc42255b5fb136b87db9
ChanHaeng Lee <2chanhaeng@gmail.com>
2023-07-10 15:49:51 +0900

    feat: swagger API Bearer Auth 적용

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, UseGuards, Param } from "@nestjs/common";
+import { ApiBearerAuth } from "@nestjs/swagger";
 import { ApiTags } from "@nestjs/swagger";
 import { JwtAuthGuard } from "~/jwt/jwt.guard";
+import { UsernameDto } from "~/dto/property.dto";
 import { UserService } from "./user.service";
 
 @ApiTags("User")
@@ -13,9 +15,10 @@ export class UserController {
     return this.userService.users();
   }
 
+  @ApiBearerAuth("access")
   @UseGuards(JwtAuthGuard)
   @Get(":username")
-  async findOne(@Param() where: { username: string }) {
+  async findOne(@Param() where: UsernameDto) {
     // DEBUG
     return this.userService.user(where);
   }
